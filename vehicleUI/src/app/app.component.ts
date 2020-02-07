3b26240f4f96297b3e6c9b37211f47bb3546552b
alimohammadahmadi <alimohammad.ahmadi2008@gmail.com>
2020-02-07 14:03:48 -0500

    Angular project created

---
+++ vehicleUI/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <h1>Search Vehicle</h1>
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'vehicleUI';
+}
