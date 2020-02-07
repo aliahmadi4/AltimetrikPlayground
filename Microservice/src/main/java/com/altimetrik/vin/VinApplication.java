cb0afbcefa564499109f1311836e449d696ade2d
aliahmadi4cs <aliahmadi4cs@WORKSPACE.ALTIMETRIK.COM>
2020-02-07 09:23:30 -0600

    project initialized

---
+++ Microservice/src/main/java/com/altimetrik/vin/VinApplication.java
@@ -0,0 +1,13 @@
+package com.altimetrik.vin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VinApplication.class, args);
+    }
+
+}
